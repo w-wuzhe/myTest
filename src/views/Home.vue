6e0ca24589989d31327bcd4d824a25054769b51a
武哲 <714225563@qq.com>
2021-02-01 14:37:31 +0800

    测试第三次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,22 @@
         home
         home
         home
+        <el-button @click="user">跳转</el-button>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
 
+        }
+    } ,
+    methods : {
+        // user() {
+        //     this.$router.push('/user')
+        // }
+    }
 }
 </script>
 
